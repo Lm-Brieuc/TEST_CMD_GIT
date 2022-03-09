83a0935442758018a6da35f1507896cf8f793d7c
Brieuc <brieucx2003@gmail.com>
2022-03-09 15:22:53 +0100

    Debut projet. Main. Calcul et Affichage

--- TEST_CMD_GIT/Program.cs
+++ TEST_CMD_GIT/Program.cs
@@ -10,6 +10,29 @@ namespace TEST_CMD_GIT
     {
         static void Main(string[] args)
         {
+            int valeur1 = 1;
+            int valeur2 = 21;
+            int result = Calc(valeur1, valeur2);
+
+            Affiche_Resultat(result);
+        }
+
+
+
+
+
+
+
+
+
+        static int Calc(int P_valeur1, int P_valeur2)
+        {
+            return P_valeur1 + P_valeur2;
+        }
+
+        static void Affiche_Resultat(int P_result)
+        {
+            Console.WriteLine(P_result);
         }
     }
 }
